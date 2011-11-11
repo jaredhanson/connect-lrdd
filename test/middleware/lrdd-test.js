99c7b371e54e9bc1602712ad77b52853466ffc37
Jared Hanson <jaredhanson@gmail.com>
2011-11-10 23:29:03 -0800

    Test formatting of XRD document.

--- test/middleware/lrdd-test.js
+++ test/middleware/lrdd-test.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 var util = require('util');
 var lrdd = require('middleware/lrdd');
 var Descriptor = require('descriptor');
+var Link = require('link');
 
 
 function MockRequest() {
@@ -29,12 +30,21 @@ vows.describe('lrdd').addBatch({
     topic: function() {
       return lrdd(function() {
         var desc = new Descriptor();
+        desc.setExpires(new Date("Wed, 09 Aug 1995 00:00:00 GMT"));
         desc.setSubject('http://example.com/gpburdell');
         desc.addAlias('http://people.example.com/gpburdell');
         desc.addAlias('acct:gpburdell@example.com');
         desc.addProperty('http://spec.example.net/type/person');
         desc.addProperty('http://spec.example.net/version', '1.0');
         desc.addProperty('http://spec.example.net/version', '2.0');
+        desc.addLink('http://services.example.com/auth', 'http://spec.example.net/auth/1.0');
+        desc.addLink({ template: 'https://example.com/lrdd/?uri={uri}', rel: 'lrdd', type: 'application/xrd+xml' });
+        var link = new Link('http://photos.example.com/gpburdell.jpg', 'http://spec.example.net/photo/1.0', 'image/jpeg');
+        link.addTitle('User Photo');
+        link.addTitle('Benutzerfoto', 'de');
+        link.addProperty('http://spec.example.net/created/1.0', '1970-01-01');
+        link.addProperty('http://spec.example.net/type/place');
+        desc.addLink(link);
         
         return desc;
       });
@@ -64,8 +74,25 @@ vows.describe('lrdd').addBatch({
       'should set Content-Type header' : function(err, req, res) {
         assert.equal(res._headers['Content-Type'], 'application/xrd+xml');
       },
-      'should set location in response data' : function(err, req, res) {
-        //assert.equal(res._data, 'x');
+      'should format XRD data correctly' : function(err, req, res) {
+        assert.equal(res._data, '<?xml version="1.0" encoding="UTF-8"?>\
+<XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">\
+<Expires>1995-08-09T00:00:00Z</Expires>\
+<Subject>http://example.com/gpburdell</Subject>\
+<Alias>http://people.example.com/gpburdell</Alias>\
+<Alias>acct:gpburdell@example.com</Alias>\
+<Property type="http://spec.example.net/type/person" xsi:nil="true" />\
+<Property type="http://spec.example.net/version">1.0</Property>\
+<Property type="http://spec.example.net/version">2.0</Property>\
+<Link rel="http://spec.example.net/auth/1.0" href="http://services.example.com/auth"></Link>\
+<Link rel="lrdd" template="https://example.com/lrdd/?uri={uri}" type="application/xrd+xml"></Link>\
+<Link rel="http://spec.example.net/photo/1.0" href="http://photos.example.com/gpburdell.jpg" type="image/jpeg">\
+<Title>User Photo</Title>\
+<Title xml:lang="de">Benutzerfoto</Title>\
+<Property type="http://spec.example.net/created/1.0">1970-01-01</Property>\
+<Property type="http://spec.example.net/type/place" xsi:nil="true" />\
+</Link>\
+</XRD>');
       },
     },
   },

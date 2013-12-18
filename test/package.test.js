d92b2ece7ba5aa5384f9954ea984c89fc698fe39
Jared Hanson <jaredhanson@gmail.com>
2013-12-18 08:16:52 -0800

    Update support files.

---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('connect-lrdd', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
